268941630e90e8a2abca5eb519db0c099749f6e8
Tima12052 <161930891+Tima12052@users.noreply.github.com>
2025-01-20 15:40:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function shootBullet(startX, startY, targetX, targetY) {
 }
 
 // Появление врагов через определенные интервалы
-setInterval(createEnemy, 2000);
+setInterval(createEnemy, 0000);
 
 // Основной игровой цикл
 function gameLoop() {
